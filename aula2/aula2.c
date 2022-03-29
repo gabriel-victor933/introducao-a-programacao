034cd9fbf8d1b9ad8ee3d298534d317993a6ab62
Gabriel Victor Alves Santana <102363772+gabriel-victor933@users.noreply.github.com>
2022-03-29 11:52:04 -0300

    Add files via upload

---
+++ aula2/aula2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int main()
+{   int h,b,a;
+    float temp;
+
+    //algoritmo da area do triangulo
+//    printf("insira a altura:\n");
+//    scanf("%d",&h);
+//    printf("insira a largura da base:\n");
+//    scanf("%d",&b);
+//    a = (b*h)/2;
+//    printf("a area do triagulo e %d\n",a);
+
+
+    //algoritmo para converter celsius em fahrenheit
+
+    printf("qual a temperatura em celsius?");
+    scanf("%f",&temp);
+    printf("%",temp);
+
+}
